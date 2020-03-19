625aa4f43d28e99fbb731a898de7e9b3485029f8
aagrosse <aagrock81@yahoo.com>
2020-03-19 17:09:05 -0400

    formatted public

--- public/js/searchData.js
+++ public/js/searchData.js
@@ -1,221 +1,209 @@
 // global moment 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // exhibitContainer holds all of our exhibits
     let exhibitContainer = $(".exh-container");
-    let ExhibitCategorySelect = $("#category");
     let nameContainer = $(".name-container");
-    let nameList = $("tbody");
+    let nameSelect = $("#designation");
     // Click events for the edit and delete buttons
-    $(document).on("click", "button.delete", handleExhibitDelete);
+    $(document).on("click", ".delete-name", handleExhibitDelete);
     $(document).on("click", "button.edit", handleExhibitEdit);
     // Variable to hold our exhibits
     let exhibits;
+    let url = window.location.search;
+    let exhibitId;
+    let nameId;
 
-      // Getting the initial list of Names
-      getNames();
-  
-    
+    // If we have this section in our url, we pull out the post id from the url
+    // In '?ExhibitId=1', ExhibitId is 1
+    if (url.indexOf("?ExhibitId=") !== -1) {
+        exhibitId = url.split("=")[1];
+        getPostData(exhibitId, "exhibit");
+    }
+    // Otherwise if we have an NameId in our url, preset the name select box to be our Name
+    else if (url.indexOf("?NameId=") !== -1) {
+        nameId = url.split("=")[1];
+    }
+
+    // Getting the initial list of Names
+    getNames();
+
+    getExhibits();
+
+
     // The code below handles the case where we want to get exhibits for a specific name
     // Looks for a query param in the url for NameId
-    // let url = window.location.search;
-    // let nameId;
-    // if (url.indexOf("?NameId=") !== -1) {
-    //   nameId = url.split("=")[1];
-    //   getExhibits(nameId);
-    // }
-    // // If there's no nameId we just get all exhibits as usual
-    // else {
-    //   getExhibits();
-    // }
-    
-    
+
+
     // This function grabs exhibits from the database and updates the view
     function getExhibits(name) {
         nameId = name || "";
         if (nameId) {
-          nameId = "/?NameId=" + nameId;
+            nameId = "/?NameId=" + nameId;
         }
         $.get("/api/names" + nameId, data => {
-          console.log("Exhibits", data);
-          exhibits = data;
-          if (!exhibits || !exhibits.length) {
-            displayEmpty(name);
-          }
-          else {
-            initializeRows();
-          }
+            console.log("Exhibits", data);
+            exhibits = data;
+            if (!exhibits || !exhibits.length) {
+                displayEmpty(name);
+            }
+            else {
+                initializeRows();
+            }
         });
-      }
-    
+    }
+
     // This function does an API call to delete exhibits
-      function deleteExhibit(id) {
+    function deleteExhibit(id) {
         $.ajax({
-          method: "DELETE",
-          url: "/api/exhibits/" + id
+            method: "DELETE",
+            url: "/api/exhibits/" + id
         })
-          .then(function() {
-            getExhibits(postCategorySelect.val());
-          });
-      }
-    
+            .then(function () {
+                getExhibits();
+                window.location.href = "/searchData"
+            });
+    }
+
     // InitializeRows handles appending all of our constructed post HTML inside blogContainer
-     function initializeRows() {
+    function initializeRows() {
         exhibitContainer.empty();
         let exhibitsToAdd = [];
         for (let i = 0; i < exhibits.length; i++) {
-          exhibitsToAdd.push(createNewRow(exhibits[i]));
+            if (exhibits[i].NameId === nameSelect.val()) {
+                exhibitsToAdd.push(createNewRow(exhibits[i]));
+            }
         }
         exhibitContainer.append(exhibitsToAdd);
-      }
-    
-     // This function constructs a post's HTML
-    //  function createNewRow(post) {
-    //     let formattedDate = new Date(post.createdAt);
-    //     formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-    //     let newPostCard = $("<div>");
-    //     newPostCard.addClass("card");
-    //     let newPostCardHeading = $("<div>");
-    //     newPostCardHeading.addClass("card-header");
-    //     let deleteBtn = $("<button>");
-    //     deleteBtn.text("x");
-    //     deleteBtn.addClass("delete btn btn-danger");
-    //     let editBtn = $("<button>");
-    //     editBtn.text("EDIT");
-    //     editBtn.addClass("edit btn btn-info");
-    //     let newPostTitle = $("<h2>");
-    //     let newPostDate = $("<small>");
-    //     let newPostAuthor = $("<h5>");
-    //     newPostAuthor.text("Written by: " + post.Author.name);
-    //     newPostAuthor.css({
-    //       float: "right",
-    //       color: "blue",
-    //       "margin-top":
-    //       "-10px"
-    //     });
-    //     let newPostCardBody = $("<div>");
-    //     newPostCardBody.addClass("card-body");
-    //     let newPostBody = $("<p>");
-    //     newPostTitle.text(post.title + " ");
-    //     newPostBody.text(post.body);
-    //     newPostDate.text(formattedDate);
-    //     newPostTitle.append(newPostDate);
-    //     newPostCardHeading.append(deleteBtn);
-    //     newPostCardHeading.append(editBtn);
-    //     newPostCardHeading.append(newPostTitle);
-    //     newPostCardHeading.append(newPostAuthor);
-    //     newPostCardBody.append(newPostBody);
-    //     newPostCard.append(newPostCardHeading);
-    //     newPostCard.append(newPostCardBody);
-    //     newPostCard.data("post", post);
-    //     return newExhibitCard;
-    //   }
-    
-      // This function figures out which exhibit we want to delete and then calls deletePost
-      function handleExhibitDelete() {
+    }
+
+    // This function figures out which exhibit we want to delete and then calls deletePost
+    function handleExhibitDelete() {
+        console.log(this.id);
         let currentExhibit = $(this)
-          .parent()
-          .parent()
-          .data("exhibit");
+
+            .parent()
+            .parent()
+            .data("exhibits");
         deleteExhibit(currentExhibit.id);
-      }
-    
-      // This function figures out which post we want to edit and takes it to the appropriate url
-      function handleExhibitEdit() {
+    }
+
+    // This function figures out which post we want to edit and takes it to the appropriate url
+    function handleExhibitEdit() {
         let currentExhibit = $(this)
-          .parent()
-          .parent()
-          .data("Exhibit");
+            .parent()
+            .parent()
+            .data("Exhibit");
         window.location.href = "/addData?id=" + currentExhibit.id;
-      }
-    
-      // This function displays a message when there are no exhibits
-      function displayEmpty(id) {
+    }
+
+    // This function displays a message when there are no exhibits
+    function displayEmpty(id) {
         let query = window.location.search;
         let partial = "";
         if (id) {
-          partial = " for designation #" + id;
+            partial = " for designation #" + id;
         }
         exhibitContainer.empty();
         let messageH2 = $("<h2>");
         messageH2.css({ "text-align": "center", "margin-top": "50px" });
         messageH2.html("No entries yet" + partial + ", navigate <a href='/cms" + query +
-        "'>here</a> in order to get started.");
+            "'>here</a> in order to get started.");
         exhibitContainer.append(messageH2);
-      }
+    }
 
-        // Function for creating a new list row for names
-        function createNameRow(nameData) {
-            let newTr = $("<tr>");
-            newTr.data("exhibits", nameData);
-            newTr.append("<td>" + date(nameData.createdAt) + "</td>");
-            newTr.append("<td> " + nameData.temperature + "</td>");
-            newTr.append("<td> " + nameData.PH + "</td>");
-            newTr.append("<td> " + nameData.salinity + "</td>");
-            newTr.append("<td> " + nameData.disolvedOxygen + "</td>");
-            newTr.append("<td> " + nameData.alkalinity + "</td>");
-            newTr.append("<td> " + nameData.ammonia + "</td>");
-            newTr.append("<td> " + nameData.nitrite + "</td>");
-            newTr.append("<td> " + nameData.nitrate + "</td>");
-            newTr.append("<td> " + nameData.iodine + "</td>");
-            newTr.append("<td> " + nameData.calcium + "</td>");
-            newTr.append("<td><a style='cursor:pointer;color:red' class='delete-name'>Delete Data</a></td>");
-            return newTr;
-          }
+    // Function for creating a new list row for names
+    function createExhibitRow(nameData) {
+        let newTr = $("<tr>");
+        newTr.data("exhibits", nameData);
+        newTr.append("<td>" + date(nameData.createdAt) + "</td>");
+        newTr.append("<td> " + nameData.temperature + "</td>");
+        newTr.append("<td> " + nameData.PH + "</td>");
+        newTr.append("<td> " + nameData.salinity + "</td>");
+        newTr.append("<td> " + nameData.disolvedOxygen + "</td>");
+        newTr.append("<td> " + nameData.alkalinity + "</td>");
+        newTr.append("<td> " + nameData.ammonia + "</td>");
+        newTr.append("<td> " + nameData.nitrite + "</td>");
+        newTr.append("<td> " + nameData.nitrate + "</td>");
+        newTr.append("<td> " + nameData.iodine + "</td>");
+        newTr.append("<td> " + nameData.calcium + "</td>");
+        newTr.append("<td><a style='cursor:pointer;color:red' id ='nameData.id' class='delete-name'>Delete Data</a></td>");
+        return newTr;
+    }
 
-          "2020-03-14T12:20:33.000Z"
+    // created at format (for reference)"2020-03-14T12:20:33.000Z"
+    // this function reformats the date to display it in mm/dd/yyyy format
+    function date(x) {
+        var parts = x.split('-');
+        var day = parts[2].split('', 2);
+        var newDate = parts[1] + '/' + day[0] + day[1] + '/' + parts[0];
+        return newDate
+    }
 
-          function date(x){
-            var parts = x.split('-');
-            var day = parts[2].split('',2);
-            var newDate = parts[1]+'/'+day[0]+day[1]+'/'+parts[0];
-            return newDate
-          }
 
 
-          //function i didnt use but i want to keep
-// function convertDate (x) {
-// var parts = x.split('-');
-// var day = parts[2].split('',2);
-// var newdate = parts[1]+'/'+day[0]+day[1]+'/'+parts[0];
-
-// console.log(x)
-// }
-
-        
-          // Function for retrieving names and getting them ready to be rendered to the page
-          function getNames() {
-            $.get("/api/exhibits", data => {
-              let rowsToAdd = [];
-              for (let i = 0; i < data.length; i++) {
-                rowsToAdd.push(createNameRow(data[i]));
-              }
-              renderNameList(rowsToAdd);
-              nameInput.val("");
-            });
-          }
-        
-          // A function for rendering the list of exhibits to the page
-          function renderNameList(rows) {
-            let nameList = $("tbody");
-            nameList.children().not(":last").remove();
-            nameContainer.children(".alert").remove();
-            if (rows.length) {
-              console.log(rows);
-              nameList.prepend(rows);
+    // Function for retrieving exhibits and getting them ready to be rendered to the page
+    function getExhibits() {
+        $.get("/api/exhibits", data => {
+            let rowsToAdd = [];
+            for (let i = 0; i < data.length; i++) {
+                rowsToAdd.push(createExhibitRow(data[i]));
             }
-            else {
-              renderEmpty();
-            }
-          }
-        
-          // Function for handling what to render when there are no names
-          function renderEmpty() {
-            let alertDiv = $("<div>");
-            alertDiv.addClass("alert alert-danger");
-            alertDiv.text("You must add data for this system.");
-            nameContainer.append(alertDiv);
-          }
-    
-    });
-  
+            renderExhibitList(rowsToAdd);
+
+        });
+    }
+
+    // A function for rendering the list of exhibits to the page
+    function renderExhibitList(rows) {
+        let nameList = $("tbody");
+        nameList.children().not(":last").remove();
+        nameContainer.children(".alert").remove();
+        if (rows.length) {
+            console.log(rows);
+            nameList.prepend(rows);
+        }
+        else {
+            renderEmpty();
+        }
+    }
+
+    // Function for handling what to render when there are no names
+    function renderEmpty() {
+        let alertDiv = $("<div>");
+        alertDiv.addClass("alert alert-danger");
+        alertDiv.text("You must add data for this system.");
+        nameContainer.append(alertDiv);
+    }
+
+    // A function to get Names and then render our list of Names
+    function getNames() {
+        $.get("/api/names", renderNameList);
+    }
+    // Function to either render a list of names, or if there are none, direct the user to the page
+    // to create a name first
+    function renderNameList(data) {
+        if (!data.length) {
+            window.location.href = "/names";
+        }
+        $(".hidden").removeClass("hidden");
+        let rowsToAdd = [];
+        for (let i = 0; i < data.length; i++) {
+            rowsToAdd.push(createNameRow(data[i]));
+        }
+        nameSelect.empty();
+        console.log(rowsToAdd);
+        console.log(nameSelect);
+        nameSelect.append(rowsToAdd);
+        nameSelect.val(nameId);
+    }
+
+    // Creates the name options in the dropdown
+    function createNameRow(name) {
+        let listOption = $("<option>");
+        listOption.attr("value", name.id);
+        listOption.text(name.name);
+        return listOption;
+    }
+
+});
